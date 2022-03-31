35224f69314f3049a9df8a6a0fd78d2da3afab69
Seoyun <sylph0105@naver.com>
2022-03-31 21:58:09 +0900

    [Update] vuedongsan. Vue 데이터바인딩하기

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div>
+		<h4 :style="blue">{{locations[0]}}</h4>
+		<p style="color: red">{{ price1 }}만원</p>
+	</div>
+	<div>
+		<h4>{{locations[1]}}</h4>
+		<p>{{ price2 }}만원</p>
+	</div>
+	<div>
+		<h4>{{locations[2]}}</h4>
+		<p>{{ price3 }}만원</p>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	data(){
+		return {
+			price1 : 55,
+			price2 : 43,
+			price3 : 38,
+			blue : 'color : blue',
+			locations : ['둔산동 원룸', '갈마동 원룸', '괴정동 원룸'],
+		}
+	},
+	components: {},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
