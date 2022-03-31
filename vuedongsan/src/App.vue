73c0baad87d4085996b404083c1f91e478922865
Seoyun <sylph0105@naver.com>
2022-03-31 23:49:07 +0900

    [Update] vuedongsan. Vue 이벤트 핸들러로 click 감지하기

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,28 +1,30 @@
 <template>
 
-	<!-- <div class="navbar">
-		<a>Home</a>
-		<a>Rooms</a>
-		<a>About</a>
-	</div> -->
-
 	<div class="navbar">
 		<a v-for="(menu, i) in navbar" :key="i">{{ menu }}</a>
 	</div>
 
+	<!-- <div v-for="(room, i) in rooms" :key="i">
+		<h4>{{ room }}</h4>
+		<p>만원</p>
+	</div> -->
 
 	<div>
-		<h4 :style="blue">{{rooms[0]}}</h4>
-		<p style="color: red">{{ price1 }}만원</p>
+		<h4>{{rooms[0]}}</h4>
+		<p>{{ prices[0] }}만원</p>
+		<button @click="increase(0)">허위매물신고</button> <span>신고 수: {{ report[0] }}</span>
 	</div>
 	<div>
 		<h4>{{rooms[1]}}</h4>
-		<p>{{ price2 }}만원</p>
+		<p>{{ prices[1] }}만원</p>
+		<button @click="increase(1)">허위매물신고</button> <span>신고 수: {{ report[1] }}</span>
 	</div>
 	<div>
 		<h4>{{rooms[2]}}</h4>
-		<p>{{ price3 }}만원</p>
+		<p>{{ prices[2] }}만원</p>
+		<button @click="increase(2)">허위매물신고</button> <span>신고 수: {{ report[2] }}</span>
 	</div>
+
 
 </template>
 
@@ -33,11 +35,14 @@ export default {
 	data(){
 		return {
 			navbar : ['Home', 'Rooms', 'About'],
-			price1 : 55,
-			price2 : 43,
-			price3 : 38,
-			blue : 'color : blue',
 			rooms : ['둔산동 원룸', '갈마동 원룸', '괴정동 원룸'],
+			prices : [55, 43, 38],
+			report : [0, 0, 0],
+		}
+	},
+	methods: {
+		increase(i){
+			this.report[i]++;
 		}
 	},
 	components: {},
