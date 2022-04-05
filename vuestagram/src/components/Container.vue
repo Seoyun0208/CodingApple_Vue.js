5ef5b1535e62c37011796a3d5f90317026c24648
Seoyun <sylph0105@naver.com>
2022-04-05 19:39:48 +0900

    [Add] vuestagram. Instagram 프로젝트 생성하기 및 데이터바인딩하기

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Post :post="post" v-for="(post, i) in posts" :key="i" />
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+
+export default {
+  name: "Container",
+  data() {},
+  components: {
+    Post: Post,
+  },
+  props: {
+    posts: Array,
+  },
+};
+</script>
+
+<style>
+</style>
